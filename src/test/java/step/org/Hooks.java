80ef6f0dbb5e172a9fce706b5a09f3e1e70384e8
tamil24694 <tamil24694@gmail.com>
2022-05-21 14:51:45 +0530

    before step

--- src/test/java/step/org/Hooks.java
+++ src/test/java/step/org/Hooks.java
@@ -9,6 +9,7 @@ import org.apache.log4j.PropertyConfigurator;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import libGlobal.BaseClass;
 
 public class Hooks extends BaseClass {
@@ -40,6 +41,11 @@ public class Hooks extends BaseClass {
 	@AfterStep
 	public void git() {
 		
+	}
+	@BeforeStep
+	private void tamil() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
